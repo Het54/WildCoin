f5f4de91818b6bc4a58ea45008c13ddbb04a4c0f
Het Patel <hetpatel54@gmail.com>
2023-04-15 14:40:25 -0500

    Main Activity Backend complete

---
+++ app/src/main/java/com/het/wildcoin/RecieveActivity.kt
@@ -0,0 +1,11 @@
+package com.het.wildcoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecieveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiving)
+    }
+}
